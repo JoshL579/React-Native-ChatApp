c275657d950e0251360e77fc1ae6f858d59b3d8d
Josh Liu <Josh.Liu@Alphapay.com>
2021-08-20 10:25:16 -0700

    add env

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
+import { BASE_URL } from 'react-native-dotenv';
 
-
-export const baseUrl = 'http://192.168.1.66:5001';
-
-// export const socket = io("http://127.0.0.1:5001/chat");
+export const baseUrl = BASE_URL;
 export const socket = io(baseUrl + "/chat");
