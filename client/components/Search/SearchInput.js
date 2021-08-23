d899a4cca9d3bdaf8780e8eb18fa9857a2a13121
Josh Liu <Josh.Liu@Alphapay.com>
2021-08-23 12:34:57 -0700

    search input component

---
+++ client/components/Search/SearchInput.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { HStack, Input, Icon, Text } from 'native-base';
+import { EvilIcons } from '@expo/vector-icons';
+
+export default function SearchInput(props) {
+    const { searchUserId, handleSearchInput } = props;
+
+    return (
+        <HStack position="relative">
+            {!searchUserId &&
+                <HStack position="absolute" top={3} left={3} pointerEvents="none">
+                    <Icon
+                        as={<EvilIcons name="search" />}
+                        size={6}
+                        _light={{
+                            color: "blueGray.400",
+                        }}
+                        _dark={{
+                            color: "blueGray.50",
+                        }}
+                    />
+                    <Text
+                        _light={{
+                            color: "blueGray.400",
+                        }}
+                        _dark={{
+                            color: "blueGray.50",
+                        }}
+                    >
+                        Enter User ID
+                        </Text>
+                </HStack>
+            }
+            <Input
+                flex={1}
+                _light={{
+                    placeholderTextColor: "blueGray.400",
+                }}
+                _dark={{
+                    placeholderTextColor: "blueGray.50",
+                }}
+                value={searchUserId}
+                onChangeText={text => handleSearchInput(text)}
+                height={12}
+            />
+        </HStack>
+    )
+}
--- client/screens/Search/Search.js
+++ client/screens/Search/Search.js
@@ -1,15 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Center, VStack, HStack, Input } from 'native-base';
+import { Center, VStack, HStack, Input, Icon, Box, Text } from 'native-base';
 import { AuthContext } from '../../context/AuthContext';
 import { ChatContext } from '../../context/ChatContext';
+import SearchInput from '../../components/Search/SearchInput';
 
 export default function Search(props) {
+    const [searchUserId, setSearchUserId] = useState('')
+
+    const handleSearchInput = (text) => {
+        setSearchUserId(text)
+    }
 
     return (
         <VStack flex={1} w="100%">
-            <HStack>
-                <Input />
-            </HStack>
+            <SearchInput searchUserId={searchUserId} handleSearchInput={handleSearchInput} />
         </VStack>
     )
 }
